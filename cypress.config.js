cef56bca4b767a6158b39ed7d17689d02296d366
NikitaBez <bezmenov.nikita@gmail.com>
2023-07-09 15:41:49 +0900

    Cypress Dashboard connection

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "taanum",
   e2e: {
     baseUrl: "http://qamid.tmweb.ru/",
     setupNodeEvents(on, config) {
